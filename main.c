7a42a2415a504da556faa605674df98f9bb54ed2
kamilmachon <kamilmachon@gmail.com>
2016-05-05 21:14:47 +0200

    Added files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "wikipedia_funkcje.h"
+
+int main(void)
+{
+	FILE* wp;
+	wp=open(void); //funkcja "open" otwiera plik o podanej przez uøytkownika nazwie i zwraca wskaünik na plik 
+	return 0;
+}
