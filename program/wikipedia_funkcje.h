40f35fab6d110d644d1d39e0da9f0c0120e281c5
Zofija <zofija96@gmail.com>
2016-05-12 00:17:08 +0200

    Wysyłam "Wybór fizycznego miejsca przechowywania plików". Postanowiłam także uporządkować nieco repozytorium i dlatego stworzyłam foldery dotyczące osobnych tematów.

---
+++ program/wikipedia_funkcje.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef WIKIPEDIA_H
+#define WIKIPEDIA_H
+int open(FILE* wp, char* nazwa)
+#endif
--- wikipedia_funkcje.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifndef WIKIPEDIA_H
-#define WIKIPEDIA_H
-int open(FILE* wp, char* nazwa)
-#endif
