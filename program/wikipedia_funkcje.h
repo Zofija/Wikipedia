a28de70e964019efbebb62309196b9e456c5b588
Zofija <zofija96@gmail.com>
2016-05-16 21:47:41 +0200

    Poprawiłam dotychczas napisany program (załączone biblioteki przesunęłam tak, aby znajdowały się wewnątrz dyrektywy kompilacji warunkowej) oraz dodałam deklarację nowej funkcji szukającej słowa w tekście.

--- program/wikipedia_funkcje.h
+++ program/wikipedia_funkcje.h
@@ -1,7 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #ifndef WIKIPEDIA_H
 #define WIKIPEDIA_H
-int open(FILE* wp, char* nazwa)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int open(FILE* wp, char* nazwa);
+void szukaj_w_tekscie(char *slowo, char *tekst);
+
 #endif
