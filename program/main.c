cfa26f0c455fc70fa0638f2f335d361d38ad3922
Zofija <zofija96@gmail.com>
2016-05-17 23:28:31 +0200

    Dodałam "słowo" oraz test funkcji "szukaj(...)"
    Dodałam możliwość podania przez użytkownika słowa, które będzie szukane w tekście za pomocą funkcji "szukaj_w_tekscie". Dodałam także możliwość przetestowania tej funkcji w programie.

--- program/main.c
+++ program/main.c
@@ -5,7 +5,7 @@
 int main(void)
 {
 	int check;
-	char nazwa[30];
+	char nazwa[30], slowo [20];
 	gets(nazwa);
 	FILE* wp;
 	if((check=open(wp, nazwa))==-1)
@@ -13,5 +13,10 @@ int main(void)
 		printf("blad");
 		return -1;
 	}
+	
+	fgets(slowo, strlen(slowo)+1, stdin);
+	
+	szukaj_w_tekscie(slowo, wp, nazwa);
+	
 	return 0;
 }
