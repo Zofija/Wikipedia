17d89951542702d5c73e6caf1cc0c52ac4cad761
Zofija <zofija96@gmail.com>
2016-05-17 23:20:29 +0200

    Dopisałam definicję funkcji "szukaj

--- program/wikipedia_funkcje.c
+++ program/wikipedia_funkcje.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "wikipedia_funkcje.h"
 
 int open(FILE* wp, char* nazwa)
 {
@@ -9,4 +8,22 @@ int open(FILE* wp, char* nazwa)
 		return -1;
 	}
 	return 0;
+}
+
+void szukaj_w_tekscie(char* slowo, FILE* wp, char* nazwa)
+{
+	open(wp, nazwa);
+	char tekst[500] = {'\0'};
+	int i=0;
+	while (feof(wp)==0)
+	{
+		fscanf(wp, "%s", &tekst[i]);
+		i++;
+	}
+	fclose(plik);
+	
+	char * wynik = strstr( slowo, tekst );
+	if (wynik != NULL) 
+        printf( "Znaleziono w tekscie slowo: %s\n", wynik );
+        return;
 }
