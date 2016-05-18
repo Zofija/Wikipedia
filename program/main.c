985a089fec16c0378a0a3d10e659da012077c458
kamilmachon <kamilmachon@gmail.com>
2016-05-18 18:34:13 +0200

    Update main.c

--- program/main.c
+++ program/main.c
@@ -1,22 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "wikipedia_funkcje.h"
 
 int main(void)
 {
-	int check;
-	char nazwa[30], slowo [20];
-	gets(nazwa);
-	FILE* wp;
-	if((check=open(wp, nazwa))==-1)
-	{
-		printf("blad");
-		return -1;
-	}
-	
-	fgets(slowo, strlen(slowo)+1, stdin);
-	
-	szukaj_w_tekscie(slowo, wp, nazwa);
-	
+	int ilosc_slow;
+	ilosc_slow=szukaj_w_tekscie();
 	return 0;
 }
