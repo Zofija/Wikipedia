7d0426a8f7c29b6cb9dcad9ddfb2d78b69470da7
Zofija <zofija96@gmail.com>
2016-05-20 16:37:25 +0200

    Dodałam liczniki i zarysowałam wygląd pętli przechodzącej przez wszystkie kategorie z słowa z nich. Nie przetestowałam

--- program/main.c
+++ program/main.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-	int ilosc_slow=0;
+	int ilosc_slow=0, i, j;
+	int licz[5]; //5 liczników, do sprawdzenia ka¿dego z 5 s³ów z danej kategorii
 	
 	char nazwa[30], slowo [20];
 	gets(nazwa);
@@ -13,10 +14,21 @@ int main(void)
 		printf("blad");
 		return -1;
 	}
+	
 	while (feof(klucze)==0)
 	{
-		fscanf(klucze, "%s", slowo);
-		ilosc_slow=szukaj_w_tekscie(slowo);
+		for( i=0; i< 20; i++ ) //pêtla przechodzi przez wszystkie kategorie
+		{
+			printf("sprawdzanie slow kluczy dla %d kategorii", i+1);
+			
+			for(j=0; j<5; j++) //pêtla przechodi przez wszystkie s³owa klucze z danej kategorii
+			{
+				fscanf(klucze, "%s", slowo);
+				ilosc_slow=szukaj_w_tekscie(slowo);
+				licz[j]=ilosc_slow; //zapisanie iloœci wyst¹pienia s³owa do odpowiedniego licznika
+			}
+		}
+		
 	}
 	
 	return 0;
